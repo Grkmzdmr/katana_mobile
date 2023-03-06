39535c3a394ae5668bda2399dffc82826b2e811a
Elif Özkan <elffozkann@gmail.com>
2023-03-06 20:51:27 +0300

    homepage

--- lib/core/constants/palette.dart
+++ lib/core/constants/palette.dart
@@ -9,5 +9,4 @@ class Palette {
   static const Color gradientSecond = Color(0xff33001b);
   static const Color white = Color.fromARGB(255, 255, 255, 255);
   static const Color black = Color.fromARGB(255, 0, 0, 0);
-  static const Color newPrimary = Color.fromARGB(255, 255, 0, 115);
 }
