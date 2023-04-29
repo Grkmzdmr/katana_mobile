ee5344601f14b21e31d3156c68a2bc8556e5abd5
Görkem Özdemir <55491306+Grkmzdmr@users.noreply.github.com>
2023-04-29 09:46:30 +0300

    Update strings.dart

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -1,5 +1,5 @@
 class Strings {
-  static const String baseUrl = "https://katana.snowsparrow.com/";
+  static const String baseUrl = "https://write-your-api-adress/";
   static const String loginUrl = baseUrl + "account/login";
 
 
